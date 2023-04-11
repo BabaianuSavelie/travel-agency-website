023db4442ec53f07ff16fc9e5431c27d18f8764d
BabaianuSavelie <babaianu.savelie1@gmail.com>
2023-04-11 08:42:32 +0300

    Initial commit

--- scripts/main.js
+++
@@ -1,8 +0,0 @@
-const navbarMenu = document.querySelector(".navbar-menu");
-const toggleButton = document.querySelector(".navbar-burger");
-
-const toggleMenu = () => {
-  navbarMenu.classList.toggle("is-active");
-};
-
-toggleButton.addEventListener("click", () => toggleMenu());
